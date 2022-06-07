85808b36519cc83d5d114afba1b639fa132254b7
Caroline Vieira <caroline@fetch.ly>
2022-06-07 23:15:17 +0200

    Dark button theme design

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,17 @@
 import { Home } from "./components/pages/Home";
+import { Sun } from "phosphor-react";
+import { Moon } from "phosphor-react";
 
 
 export function App(){
-  return <Home />
+  return (
+    <div>
+      <button className="bg-[#8257E5] w-20 h-120 rounded-full border-4 flex justify-center p-2">
+        <Sun size={24} color="#EAC435" />
+        <Moon size={24} color="#345995" /> 
+      </button>
+      <Home />
+    </div>
+
+  )
 }
