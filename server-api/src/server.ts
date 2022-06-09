1f4b2672b0ee37d0d20c5ebef9491eb993a57236
Caroline Vieira <caroline@fetch.ly>
2022-06-09 16:22:41 +0200

    fix payload

--- server-api/src/server.ts
+++ server-api/src/server.ts
@@ -4,7 +4,15 @@ import cors from 'cors'
 import { routes } from './routes';
 
 const app = express();
-
+app.use(express.json({ limit: '50mb' }));
+app.use(
+    express.urlencoded({
+        limit: '200mb',
+        extended: true,
+        parameterLimit: 50000,
+    }),
+);
+app.use(express.text({ limit: '200mb' }));
 app.use(cors())
 app.use(express.json());
 app.use(routes);
